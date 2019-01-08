2644d7852617e935c9bc453b44bcdcbe53229973
root <root@hannan-OptiPlex-3050>
2019-01-08 11:42:35 +0600

    initialization

---
+++ src/algorithm/QuickSort/QuickSortTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithm.QuickSort;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author root
+ */
+public class QuickSortTest {
+    public static void main(String[] args) {
+        int arr[] = {9, -3, 5, 2, 6, 8, -6, 1, 3};
+        quickSort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void quickSort(int[] arr, int start, int end) {
+        if(start >= end )
+        {
+            return;
+        }
+        int pivot = partition(arr, start, end);
+        quickSort(arr, start, pivot-1);
+        quickSort(arr, pivot+1, end);
+    }
+
+    private static int partition(int[] arr, int start, int end) {
+        int pIndex = start;
+        int pivot = arr[end];
+        
+        for(int i = start; i < end; i++)
+        {
+            if(arr[i] <= pivot)
+            {
+                swap(arr, i, pIndex);
+                pIndex++;
+            }
+        }
+        swap(arr, end, pIndex);
+        
+        return pIndex;
+    }
+
+    private static void swap(int[] arr, int i, int pIndex) {
+        int temp = arr[i];
+        arr[i] = arr[pIndex];
+        arr[pIndex] = temp;
+    }
+}
